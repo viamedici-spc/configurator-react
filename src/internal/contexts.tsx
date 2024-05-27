11547b1e7a380e8c93294675111d8a010b44db46
birmitt <13151231+birmitt@users.noreply.github.com>
2024-05-27 09:54:32 +0200

    Store the React contexts in the global context, allowing them to be used from Framer Code Components. There is an issue with importing the contexts-Module as the same instance as the library does.

--- src/internal/contexts.tsx
+++ src/internal/contexts.tsx
@@ -1,10 +1,26 @@
-import {createContext, useContext} from "react";
+import {Context, createContext, useContext} from "react";
 import {ConfigurationInitialization, ConfigurationUpdating} from "../types";
 import {IConfigurationSession, Configuration} from "@viamedici-spc/configurator-ts";
 
-export const ConfigurationSessionContext = createContext<IConfigurationSession>(null);
-export const ConfigurationInitializationContext = createContext<ConfigurationInitialization>(null);
-export const ConfigurationUpdatingContext = createContext<ConfigurationUpdating>(null);
-export const ConfigurationContext = createContext<Configuration>(null);
+type Contexts = {
+    ConfigurationSessionContext: Context<IConfigurationSession>;
+    ConfigurationInitializationContext: Context<ConfigurationInitialization>;
+    ConfigurationUpdatingContext: Context<ConfigurationUpdating>;
+    ConfigurationContext: Context<Configuration>;
+}
+
+const contexts: Contexts = (window as any).configuratorReactContexts ?? {
+    ConfigurationSessionContext: createContext(null),
+    ConfigurationInitializationContext: createContext(null),
+    ConfigurationUpdatingContext: createContext(null),
+    ConfigurationContext: createContext(null)
+} satisfies Contexts;
+(window as any).configuratorReactContexts = contexts;
+
+export const ConfigurationSessionContext = contexts.ConfigurationSessionContext;
+export const ConfigurationInitializationContext = contexts.ConfigurationInitializationContext;
+export const ConfigurationUpdatingContext = contexts.ConfigurationUpdatingContext;
+export const ConfigurationContext = contexts.ConfigurationContext;
+
 export const useConfigurationSessionContext = () => useContext(ConfigurationSessionContext);
 export const useConfigurationContext = () => useContext(ConfigurationContext);
